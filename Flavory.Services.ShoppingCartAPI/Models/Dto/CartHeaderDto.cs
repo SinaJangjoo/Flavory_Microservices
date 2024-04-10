efcbfbe6e6630bbc0e832cd3f34cc4fe3547253a
Sina Jangjoo <Sina Jangjoo@DESKTOP-LNFK5GK>
2024-04-10 14:04:25 +0330

    Modify all Models and Dtos inside ShoppingCartAPI

---
+++ Flavory.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Flavory.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartHeaderDto
+    {
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double CartTotal { get; set; }
+    }
+}
---
+++ Flavory.Services.ShoppingCartAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Flavory.Services.ShoppingCartAPI.Models.Dto
+{
+	public class CouponDto
+	{
+		public int CouponId { get; set; }
+		public string CouponCode { get; set; }
+		public double DiscountAmount { get; set; }
+		public int MinAmount { get; set; }
+	}
+}
