cc4886585e6f052c09e994517ae06aa366ac6ec1
Sina Jangjoo <Sina Jangjoo@DESKTOP-LNFK5GK>
2024-04-10 14:32:31 +0330

    Add ShoppingCart Tables to Database and setup basic configuration

---
+++ Flavory.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Flavory.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flavory.Services.ShoppingCartAPI.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+	}
+}
---
+++ Flavory.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Flavory.Services.ShoppingCartAPI.Models;
+using Flavory.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+
+	//As long as the property name are exactly the same in both "model" and "Dto" the auto mapper do the mapping
+
+	public class MappingConfig
+	{
+		public static MapperConfiguration RegisterMaps()
+		{
+			var mappingConfig = new MapperConfiguration(config =>
+			{
+				config.CreateMap<CartHeader,CartHeaderDto>().ReverseMap();  // <Source , Destination>();
+				config.CreateMap<CartDetails,CartDetailsDto>().ReverseMap();  // <Source , Destination>();
+			});
+			return mappingConfig;
+		}
+	}
+}
